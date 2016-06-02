8cd4e931ac33cd7e58c4213da8b063ea49d17418
Mudit Jain <mudjain18@gmail.com>
2016-06-02 15:39:26 +0530

    Testing SourceTree

---
+++ SampleTest/SampleTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SampleTest
+//
+//  Created by Mudit Jain on 02/06/16.
+//  Copyright © 2016 Persistent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hi there")
+            print("Hi there")
+            print("Hi there")
+        print("Hi there")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
