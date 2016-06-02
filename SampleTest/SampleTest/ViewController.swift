f4f1595bc163eb02d8a74ad61ff72cce86860476
Mudit Jain <mudjain18@gmail.com>
2016-06-02 15:44:44 +0530

    Trying to add conflict

--- SampleTest/SampleTest/ViewController.swift
+++ SampleTest/SampleTest/ViewController.swift
@@ -15,8 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Hi there")
             print("Hi there")
-            print("Hi there")
-        print("Hi there")
+
+
     }
 
     override func didReceiveMemoryWarning() {
